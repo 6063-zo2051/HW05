b3d5d735eee0f8a49f781832957af67c2044bfdc
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-10-11 15:03:46 -0400

    HW05 Iteration 3
    Error fixed and some detail added to graph.

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 The goal of this assignment was to create a data visualization from a chosen dataset.
 ---
 
+Update #1:
+
 Out of interest, I chose a dataset that I found on Kaggle which had information on food delivery times. I wanted to make a simple bar chart that showed the relationship between age of the deliverer and the time takemn to deliver the food. From there, I wanted to play with the intricacy of the graph itself. I decided to use the first 1000 data points to represent. For longer delivery times, I wanted to make a 
 
 https://www.kaggle.com/datasets/rajatkumar30/food-delivery-time/
@@ -9,3 +11,7 @@ https://www.kaggle.com/datasets/rajatkumar30/food-delivery-time/
 I kept running into an error at line 9 of my code, where the command window indicated a syntax error. I played around with the names and could not overcome this issue. I also realized that a plot chart would have been more appropriate for this information, and maybe that is where my error was coming in.
 
 Nevertheless, I tried to use the code from the tutorial of the loading process instead, and still ran into my error. This endured even with including "csv", "header" and excluding it. I also tried putting the headers here and was overall confused.
+
+Update #2:
+
+I was able to figure out my error, and wanted to again try my graph, altering it into a scatter plot this time as I felt that would better represent the data. I also made the points themselves into tiny pizzas to coincide with the food delivery theme. I was able to create the plot; however, I could not figure out how to make my chart larger for viewing or to shift it upwards to bo be so low in the window. I also would have liked to had labels to clarify the ages and delivery times.
--- sketch.js
+++ sketch.js
@@ -1,69 +1,38 @@
 let data;
 
-let ageMin;
-let ageMax;
-let timeMin;
-let timeMax;
-
 function preload() {
   data = loadTable("./deliverytime.csv", "csv", "header");
 }
 
 function setup() {
-
   createCanvas(windowWidth, windowHeight);
+  background(255);
+  noStroke();
 }
  
-function setup() {
-  createCanvas(400, 400);
+function draw() {
+
+  let dataPoints = data.getRowCount();
 
   for (let i = 0; i < 1000; i++) {
     let age = data.getNum(i, "Delivery_person_Age");
     let time = data.getNum(i, "Time_taken(min)");
+let x = map(age, 10, 50, 5, width - 5);
+let y = map(time, 0, 100, height - 5, 5);
+fill(236, 200, 136);
+circle (x, y, 7);
+fill(255, 226, 7);
+circle (x, y, 5);
+fill(255, 0, 0);
+circle (x - 1, y - 1, 2);
+circle (x + 1, y + 1, 2);
+circle (x - 1, y + 1, 1);
+circle (x + 1, y - 1, 1);
 
+fill(0, 0, 0);
+textSize(12);
+text("Age (years)", width/2.5, height - 50);
+text("Delivery Time (min)", 5, height/2);
 
-    ageMin = min(ageMin, age);
-    ageMax = max(ageMax, age);
-    timeMin = min(timeMin, time);
-    timeMax = max(timeMax, time);
-  }
 }
-
-function draw() {
-  background(0);
-  noFill();
-  stroke(190, 20, 255);
-  for (let i = 0; i < 1000; i++) {
-    let age = data.get(i, "Delivery_person_Age");
-    let time = data.get(i, "Time_taken(min)");
-    let x = map(age, ageMin, ageMax, 0, width);
-    let d = map(time, timeMin, timeMax, 20, width);
-    ellipse(x, height / 2, d, d);
-  }
-  noLoop();
 }
-
-
-  //let age = [];
-  //let deliveryTime = [];
-
- // for (let i = 0; i < 1000; i++) {
-  //  let age = data.getNum(i, "Delivery_person_Age");
-  //  let time = data.getNum(i, "Time_taken(min)");
- // }
-//}
-
-//function draw() {
- //background(255);
-
-  //let barWidth = width/1000;
-
- // for ( let i = 0, i < data.length; i++);
-  //let x = map(i, 0, data.length, 0, width);
- // let y = map(data[i].deliveryTime, 0, timeMax, height, 0);
- // let barHeight = height - y;
-
-  //fill(255, 0, 0);
- // rect(x, y, barWidth, barHeight);
-
-//}
